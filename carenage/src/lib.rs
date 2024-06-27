4e2ab63d18572e51a6b6bcb4630b2011767c30c6
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-06-27 17:51:41 +0200

    feat: error handling if 500 received from boagent, boagent_url as query_boagent parameter

--- carenage/src/lib.rs
+++ carenage/src/lib.rs
@@ -1,46 +1,49 @@
-use reqwest::Result;
-use reqwest::blocking::Client;
+use reqwest::blocking::{Client, Response};
 
-fn query_boagent(start_time: Option<u64>, end_time: Option<u64>, location: String, lifetime: u8) -> Result<u16> {
+fn query_boagent(
+    boagent_url: String,
+    start_time: Option<u64>,
+    end_time: Option<u64>,
+    location: String,
+    lifetime: u8,
+) -> Result<Response, String> {
     let client = Client::new();
-    let base_url = format!("http://127.0.0.1:8000/query");    
-    let response = client.get(base_url)
-                    .query(&[("start_time", start_time.expect("fail").to_string())])   
-                    .query(&[("end_time", end_time.expect("fail").to_string())])
-                    .query(&[("verbose", "true")])
-                    .query(&[("location", location)])
-                    .query(&[("measure_power", "true")])
-                    .query(&[("lifetime", lifetime)])
-                    .query(&[("fetch_hardware", "true")])
-                    .send()?;
-        
-        
+    let base_url = format!("{}/query", boagent_url);
+    let query_parameters = vec![
+        ("start_time", start_time.expect("unable to parse timestamp").to_string()),
+        ("end_time", end_time.expect("unable to parse timestamp").to_string()),
+        ("verbose", "true".to_string()),
+        ("location", location),
+        ("measure_power", "true".to_string()),
+        ("lifetime", lifetime.to_string()),
+        ("fetch_hardware", "true".to_string()),
+    ];
+    let response = client
+        .get(base_url)
+        .query(&query_parameters)
+        .send()
+        .expect("Failure to execute request.");
 
     match response.status().as_u16() {
-        200 => {
-            println!("OK");
-            Ok(200)
-        }
-        _ => {
-            println!("Error");
-            Ok(response.status().as_u16())
-        }
+        200 => Ok(response),
+        _ => Err("Error from Boagent.".to_string()),
     }
 }
 
-
-
 #[cfg(test)]
 mod tests {
+    use super::*;
     use mockito::{Matcher, Server};
     use std::time::SystemTime;
-    use super::*;
 
     #[test]
     fn it_queries_boagent_with_success_with_needed_query_paramaters() {
+        let now_timestamp = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let now_timestamp_minus_one_minute = now_timestamp - 60;
 
-        let now_timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-        let now_timestamp_minus_one_minute = now_timestamp - 60; 
         let opts = mockito::ServerOpts {
             host: "127.0.0.1",
             port: 8000,
@@ -49,7 +52,8 @@ mod tests {
 
         let mut boagent_server = Server::new_with_opts(opts);
 
-        let mock = boagent_server.mock("GET", "/query")
+        let _mock = boagent_server
+            .mock("GET", "/query")
             .match_query(Matcher::AllOf(vec![
                 Matcher::Regex(format!("start_time={:?}", now_timestamp_minus_one_minute).into()),
                 Matcher::Regex(format!("end_time={:?}", now_timestamp).into()),
@@ -57,14 +61,61 @@ mod tests {
                 Matcher::Regex("location=FRA".into()),
                 Matcher::Regex("measure_power=true".into()),
                 Matcher::Regex("lifetime=5".into()),
-                Matcher::Regex("fetch_hardware=true".into())
+                Matcher::Regex("fetch_hardware=true".into()),
             ]))
             .with_status(200)
             .create();
 
-        let response = query_boagent(Some(now_timestamp_minus_one_minute), Some(now_timestamp), "FRA".to_string(), 5).unwrap(); 
-        mock.assert();
-        assert_eq!(response, 200);
+        let response = query_boagent(
+            "http://127.0.0.1:8000".to_string(),
+            Some(now_timestamp_minus_one_minute),
+            Some(now_timestamp),
+            "FRA".to_string(),
+            5,
+        )
+        .unwrap();
+
+        assert_eq!(response.status().as_u16(), 200);
     }
 
+    #[test]
+    fn it_queries_boagent_with_error_if_500_http_code_received_from_boagent() {
+        let now_timestamp = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let now_timestamp_minus_one_minute = now_timestamp - 60;
+
+        let opts = mockito::ServerOpts {
+            host: "127.0.0.1",
+            port: 8001,
+            ..Default::default()
+        };
+
+        let mut boagent_server = Server::new_with_opts(opts);
+
+        let _mock = boagent_server
+            .mock("GET", "/query")
+            .match_query(Matcher::AllOf(vec![
+                Matcher::Regex(format!("start_time={:?}", now_timestamp_minus_one_minute).into()),
+                Matcher::Regex(format!("end_time={:?}", now_timestamp).into()),
+                Matcher::Regex("verbose=true".into()),
+                Matcher::Regex("location=FRA".into()),
+                Matcher::Regex("measure_power=true".into()),
+                Matcher::Regex("lifetime=5".into()),
+                Matcher::Regex("fetch_hardware=true".into()),
+            ]))
+            .with_status(500)
+            .create();
+
+        let response = query_boagent(
+            "http://127.0.0.1:8001".to_string(),
+            Some(now_timestamp_minus_one_minute),
+            Some(now_timestamp),
+            "FRA".to_string(),
+            5,
+        );
+
+        assert_eq!(response.is_err(), true);
+    }
 }
