669d0502e67f59e15a5d77ce2bfd21fe0a532387
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-07-01 17:39:42 +0200

    feat: handling unix and iso 8601 timestamps for boagent query

--- carenage/src/lib.rs
+++ carenage/src/lib.rs
@@ -1,28 +1,43 @@
 use reqwest::blocking::{Client, Response};
+use chrono::{DateTime, Local, TimeZone};
+
+enum Timestamp {
+    UnixTimestamp(Option<u64>),
+    ISO8601Timestamp(DateTime<Local>),
+}
 
 fn query_boagent(
     boagent_url: String,
-    start_time: Option<u64>,
-    end_time: Option<u64>,
+    start_time: Timestamp,
+    end_time: Timestamp,
     location: String,
     lifetime: u8,
 ) -> Result<Response, String> {
+    
+    let mut query_parameters = vec![];
+
+    match start_time {
+        Timestamp::UnixTimestamp(start_time) => {query_parameters.push(("start_time", start_time.unwrap_or(0).to_string()))},
+        Timestamp::ISO8601Timestamp(start_time) => {query_parameters.push(("start_time", format!("{:?}", start_time)))}
+    }
+    match end_time {
+        Timestamp::UnixTimestamp(end_time) => {query_parameters.push(("end_time", end_time.unwrap_or(0).to_string()))},
+        Timestamp::ISO8601Timestamp(end_time) => {query_parameters.push(("end_time", format!("{:?}", end_time)))}
+    }
+    query_parameters.push(("verbose", "true".to_string()));
+    query_parameters.push(("location", location));
+    query_parameters.push(("measure_power", "true".to_string()));
+    query_parameters.push(("lifetime", lifetime.to_string()));
+    query_parameters.push(("fetch_hardware", "true".to_string()));
+
     let client = Client::new();
     let base_url = format!("{}/query", boagent_url);
-    let query_parameters = vec![
-        ("start_time", start_time.expect("unable to parse timestamp").to_string()),
-        ("end_time", end_time.expect("unable to parse timestamp").to_string()),
-        ("verbose", "true".to_string()),
-        ("location", location),
-        ("measure_power", "true".to_string()),
-        ("lifetime", lifetime.to_string()),
-        ("fetch_hardware", "true".to_string()),
-    ];
+
     let response = client
         .get(base_url)
         .query(&query_parameters)
         .send()
-        .expect("Failure to execute request.");
+        .unwrap();
 
     match response.status().as_u16() {
         200 => Ok(response),
@@ -35,6 +50,7 @@ mod tests {
     use super::*;
     use mockito::{Matcher, Server};
     use std::time::SystemTime;
+    use chrono::{Duration, Local};
 
     #[test]
     fn it_queries_boagent_with_success_with_needed_query_paramaters() {
@@ -44,13 +60,9 @@ mod tests {
             .as_secs();
         let now_timestamp_minus_one_minute = now_timestamp - 60;
 
-        let opts = mockito::ServerOpts {
-            host: "127.0.0.1",
-            port: 8000,
-            ..Default::default()
-        };
+        let mut boagent_server = Server::new();
 
-        let mut boagent_server = Server::new_with_opts(opts);
+        let url = boagent_server.url();
 
         let _mock = boagent_server
             .mock("GET", "/query")
@@ -67,9 +79,9 @@ mod tests {
             .create();
 
         let response = query_boagent(
-            "http://127.0.0.1:8000".to_string(),
-            Some(now_timestamp_minus_one_minute),
-            Some(now_timestamp),
+            url,
+            Timestamp::UnixTimestamp(Some(now_timestamp_minus_one_minute)),
+            Timestamp::UnixTimestamp(Some(now_timestamp)),
             "FRA".to_string(),
             5,
         )
@@ -86,13 +98,9 @@ mod tests {
             .as_secs();
         let now_timestamp_minus_one_minute = now_timestamp - 60;
 
-        let opts = mockito::ServerOpts {
-            host: "127.0.0.1",
-            port: 8001,
-            ..Default::default()
-        };
+        let mut boagent_server = Server::new();
 
-        let mut boagent_server = Server::new_with_opts(opts);
+        let url = boagent_server.url();
 
         let _mock = boagent_server
             .mock("GET", "/query")
@@ -109,13 +117,81 @@ mod tests {
             .create();
 
         let response = query_boagent(
-            "http://127.0.0.1:8001".to_string(),
-            Some(now_timestamp_minus_one_minute),
-            Some(now_timestamp),
+            url,
+            Timestamp::UnixTimestamp(Some(now_timestamp_minus_one_minute)),
+            Timestamp::UnixTimestamp(Some(now_timestamp)),
             "FRA".to_string(),
             5,
         );
 
         assert_eq!(response.is_err(), true);
+    }
+    
+    #[test]
+    fn it_queries_boagent_with_success_with_unspecified_timestamps() {
+
+        let mut boagent_server = Server::new();
+
+        let url = boagent_server.url();
+
+        let _mock = boagent_server
+            .mock("GET", "/query")
+            .match_query(Matcher::AllOf(vec![
+                Matcher::Regex("start_time=0".into()),
+                Matcher::Regex("end_time=0".into()),
+                Matcher::Regex("verbose=true".into()),
+                Matcher::Regex("location=FRA".into()),
+                Matcher::Regex("measure_power=true".into()),
+                Matcher::Regex("lifetime=5".into()),
+                Matcher::Regex("fetch_hardware=true".into()),
+            ]))
+            .with_status(200)
+            .create();
+
+        let response = query_boagent(
+            url,
+            Timestamp::UnixTimestamp(None),
+            Timestamp::UnixTimestamp(None),
+            "FRA".to_string(),
+            5,
+        ).unwrap();
+
+        assert_eq!(response.status().as_u16(), 200);
+    }
+
+    #[test]
+    fn it_queries_boagent_with_success_with_iso_8601_timestamps() {
+
+        let now_timestamp = Local::now(); 
+        let now_timestamp_minus_one_minute = now_timestamp - Duration::minutes(1);
+
+        let mut boagent_server = Server::new();
+
+        let url = boagent_server.url();
+
+        let mock = boagent_server
+            .mock("GET", "/query")
+            .match_query(Matcher::AllOf(vec![
+                Matcher::UrlEncoded("start_time".into(), format!("{:?}", now_timestamp_minus_one_minute).into()),
+                Matcher::UrlEncoded("end_time".into(), format!("{:?}", now_timestamp).into()),
+                Matcher::Regex("verbose=true".into()),
+                Matcher::Regex("location=FRA".into()),
+                Matcher::Regex("measure_power=true".into()),
+                Matcher::Regex("lifetime=5".into()),
+                Matcher::Regex("fetch_hardware=true".into()),
+            ]))
+            .with_status(200)
+            .create();
+
+        let response = query_boagent(
+            url,
+            Timestamp::ISO8601Timestamp(now_timestamp_minus_one_minute),
+            Timestamp::ISO8601Timestamp(now_timestamp),
+            "FRA".to_string(),
+            5,
+        ).unwrap();
+
+        mock.assert();
+        assert_eq!(response.status().as_u16(), 200);
     }
 }
