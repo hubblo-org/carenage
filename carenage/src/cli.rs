150f703d28dd750531f0a112086cb8e59c78c097
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-07-16 17:45:07 +0200

    feat: create_timestamp function

--- carenage/src/cli.rs
+++ carenage/src/cli.rs
@@ -1,12 +1,16 @@
+use crate::database::Timestamp;
+use chrono::Utc;
 use clap::{Parser, Subcommand};
+use std::time::SystemTime;
 
 #[derive(Parser)]
 pub struct Cli {
     #[command(subcommand)]
     pub event: Option<Events>,
+
     /// Format timestamp to Unix epoch, in seconds
     #[arg(short, long)]
-    pub unix: bool 
+    pub unix: bool,
 }
 
 #[derive(Parser, Debug)]
@@ -20,6 +24,19 @@ pub struct StartArgs {
 pub enum Events {
     /// Start carenage, with an optional time step
     Start(StartArgs),
+
     /// Stop carenage, final event
     Stop,
+}
+
+pub fn create_timestamp(unix: bool) -> Timestamp {
+    match unix {
+        true => Timestamp::UnixTimestamp(Some(
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+        )),
+        false => Timestamp::ISO8601Timestamp(Some(Utc::now())),
+    }
 }
