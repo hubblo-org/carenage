d2c1ec1efac440c10cdcab6b5ae05d2b559a0331
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-07-16 15:38:04 +0200

    test: query to boagent from cli

--- carenage/tests/cli.rs
+++ carenage/tests/cli.rs
@@ -2,6 +2,7 @@ use assert_cmd::prelude::*;
 use std::{process::Command, time::SystemTime};
 use chrono::Utc;
 use predicates::str::contains;
+use mockito::{Matcher, Server};
 
 // carenage start
 #[test]
@@ -38,12 +39,24 @@ fn it_accepts_start_with_a_specified_time_step_in_seconds() -> Result<(), Box<dy
 }
 
 #[test]
+fn it_fails_when_giving_an_invalid_time_step_with_start_command() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut cmd = Command::cargo_bin("carenage")?;
+    
+    cmd.arg("start").arg("--step").arg("test");
+    cmd.assert().failure();
+
+    Ok(())
+}
+
+
+#[test]
 fn it_prints_start_timestamp_in_iso_8601_format() -> Result<(), Box<dyn std::error::Error>> {
     
     // Test will probably have to be rewritten as printing the start timestamp will depend on how
     // GitHub, GitLab and other print their first timestamp in pipelines
 
-    let start_timestamp = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    let start_timestamp = Utc::now().format("%Y-%m-%dT%H:%M:%S").to_string();
     let mut cmd = Command::cargo_bin("carenage")?;
     
     cmd.arg("start");
@@ -68,6 +81,47 @@ fn it_prints_start_timestamp_in_unix_epoch_format_in_seconds_with_given_argument
     Ok(())
 }
 
+#[test]
+fn it_queries_boagent_when_calling_start_command() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("carenage")?;
+
+    let opts = mockito::ServerOpts {
+        host: "127.0.0.1",
+        port: 3000,
+        ..Default::default()
+    };
+
+    let mut boagent_server = mockito::Server::new_with_opts(opts);
+
+    let mock = boagent_server
+        .mock("GET", "/query")
+        .match_query(Matcher::AllOf(vec![
+            Matcher::Regex("verbose=true".into()),
+            Matcher::Regex("location=FRA".into()),
+            Matcher::Regex("measure_power=true".into()),
+            Matcher::Regex("lifetime=5".into()),
+            Matcher::Regex("fetch_hardware=true".into()),
+        ]))
+        .with_status(200)
+        .create();
+
+    cmd.arg("start");
+    cmd.assert().success().stdout(contains("Queried Boagent"));
+
+    mock.assert(); 
+    Ok(())
+}
+
+#[test]
+fn it_prints_error_message_when_it_fails_to_request_boagent() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("carenage")?;
+
+    cmd.arg("start");
+    cmd.assert().success().stdout(contains("Failed to query Boagent"));
+
+    Ok(())
+}
+
 // carenage stop
 #[test]
 fn it_accepts_stop_as_a_command() -> Result<(), Box<dyn std::error::Error>> {
@@ -83,7 +137,7 @@ fn it_accepts_stop_as_a_command() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 fn it_prints_stop_timestamp_in_iso_8601_format() -> Result<(), Box<dyn std::error::Error>> {
 
-    let stop_timestamp = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    let stop_timestamp = Utc::now().format("%Y-%m-%dT%H:%M:%S").to_string();
     let mut cmd = Command::cargo_bin("carenage")?;
     
     cmd.arg("stop");
