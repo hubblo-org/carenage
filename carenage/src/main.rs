9f0e0725eaa5b53f599e80ad1b6bf2adb6c02139
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-07-16 17:46:57 +0200

    refactor: using create_timestamp function

--- carenage/src/main.rs
+++ carenage/src/main.rs
@@ -1,69 +1,46 @@
-use chrono::Utc;
 use clap::Parser;
+use crate::database::Timestamp;
 use dotenv::var;
-use std::time::SystemTime;
-use crate::database::{Timestamp, query_boagent};
+use crate::database::query_boagent;
 
 pub mod cli;
 pub mod database;
 
-fn main() -> Result<(), ()> {
+fn main() {
     let cli = cli::Cli::parse();
 
-    let start_timestamp: Timestamp;
-    let stop_timestamp: Timestamp;
     let boagent_url = var("BOAGENT_URL").expect("BOAGENT_URL environment variable is absent");
     let printable_boagent_url = boagent_url.clone();
 
-    match cli.unix {
-        true => {
-            start_timestamp = Timestamp::UnixTimestamp(Some(
-                SystemTime::now()
-                    .duration_since(SystemTime::UNIX_EPOCH)
-                    .unwrap()
-                    .as_secs(),
-            ));
-            stop_timestamp = Timestamp::UnixTimestamp(Some(
-                SystemTime::now()
-                    .duration_since(SystemTime::UNIX_EPOCH)
-                    .unwrap()
-                    .as_secs(),
-            ));
-        }
-        false => {
-            start_timestamp = Timestamp::ISO8601Timestamp(Utc::now());
-            stop_timestamp = Timestamp::ISO8601Timestamp(Utc::now());
-        }
-    }
+    let start_timestamp: Timestamp;
+    let stop_timestamp: Timestamp;
 
     match &cli.event {
         Some(cli::Events::Start(step)) => {
+            start_timestamp = cli::create_timestamp(cli.unix);
+            stop_timestamp = cli::create_timestamp(cli.unix);
             println!("Carenage start event, time step of {:?} seconds", step.step);
-            println!("{:?}", start_timestamp.to_string())
+            println!("Start event timestamp is {:?}", start_timestamp.to_string());
+            let boagent_query = query_boagent(
+                boagent_url,
+                start_timestamp,
+                stop_timestamp,
+                true,
+                "FRA".to_string(),
+                5,
+            );
+                match boagent_query {
+                    Ok(response) => println!("Queried Boagent at {:?} : {:?}", printable_boagent_url, response),
+                    Err(err) => println!("Failed to query Boagent: {:?}", err),
+                };
         }
         Some(cli::Events::Stop) => {
+            let timestamp = cli::create_timestamp(cli.unix);
             println!("Carenage stop event");
-            println!("{:?}", stop_timestamp.to_string())
+            println!("Stop event timestamp is {:?}", timestamp.to_string())
         }
         None => {
             println!("Unknown command")
-        }
-    }
-
-    let boagent_query = query_boagent(
-        boagent_url,
-        start_timestamp,
-        stop_timestamp,
-        true,
-        "FRA".to_string(),
-        5,
-    );
-
-    match boagent_query {
-        Ok(response) => Ok(println!("Queried Boagent at {:?} : {:?}", printable_boagent_url, response)),
-        Err(err) => {
-            println!("Failed to query Boagent: {:?}", err);
-            Ok(())
         }
     }
 }
