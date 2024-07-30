fb66dddb03fc606a406e0aaee183b68cb9225dd7
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-07-30 17:43:01 +0200

    feat: handling PID for start and stop

--- carenage/carenage-cli/src/main.rs
+++ carenage/carenage-cli/src/main.rs
@@ -1,6 +1,7 @@
 use database::timestamp;
 use clap::Parser;
-use std::process::Command;
+use sysinfo::{Pid, Process, Signal, System};
+use std::{fs::File, io::Write, process::Command};
 
 pub mod cli;
 
@@ -26,15 +27,23 @@ fn main() {
             stop_timestamp = timestamp::Timestamp::new(cli.unix);
             println!("Carenage start event, time step of {:?} seconds", step.step);
             println!("Start event timestamp is {:?}", start_timestamp.to_string());
-            Command::new("./target/debug/carenaged").spawn().expect("failed to fork carenaged"); 
+            let carenaged = Command::new("./target/debug/carenaged").spawn().expect("Failed to fork carenaged.");
+            let carenaged_pid = carenaged.id().to_string();
+            let mut pid_file = File::create("pid.txt").expect("Failed to create file to save child process PID.");
+            pid_file.write_all(carenaged_pid.as_bytes()).expect("Failed to save child process PID in file.");
         }
         Some(cli::Events::Stop) => {
             let stop_timestamp = timestamp::Timestamp::new(cli.unix);
-
+            let system = System::new_all();
+            
             let printable_start_timestamp = start_timestamp.to_string();
             let printable_stop_timestamp = stop_timestamp.to_string();
             println!("Carenage stop event");
             println!("Stop event timestamp is {:?}", printable_stop_timestamp);
+            let pid_file = std::fs::read_to_string("pid.txt").expect("Failed to open file with saved child process PID.");
+            let pid = pid_file.parse::<u32>().expect("Failed to convert pid.txt contents to u32.");
+            let carenaged_process = system.process(Pid::from_u32(pid)).expect("Failed to get process with given PID");
+            carenaged_process.kill_with(Signal::Term).expect("Failed to kill with SIGTERM");
         }
         None => {
             println!("Unknown command")
