f258aacb1b2988ce38efb77aab78f2518603e37c
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-07-30 17:43:23 +0200

    feat: handling SIGTERM

--- carenage/carenaged/src/main.rs
+++ carenage/carenaged/src/main.rs
@@ -1,5 +1,16 @@
 use std::process;
+use tokio::signal::unix::{signal, SignalKind};
 
-fn main() {
-    println!("Started carenage daemon with PID: {}", process::id());
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut sigterm = signal(SignalKind::terminate())?;
+
+    loop {
+        println!("Started carenage daemon with PID: {}", process::id());
+        sigterm.recv().await;
+        println!("Received SIGTERM signal");
+        println!("Stopped carenage daemon.");
+        break;
+    }
+    Ok(())
 }
