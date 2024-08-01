dfe20b5f392df9c3e6e8c17010c2233b019a6958
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-08-01 16:01:16 +0200

    style: spaces between lines for readability

--- carenage/carenage-cli/src/main.rs
+++ carenage/carenage-cli/src/main.rs
@@ -16,17 +16,22 @@ fn main() {
     match &cli.event {
         Some(cli::Events::Start(step)) => {
             let start_timestamp: timestamp::Timestamp = timestamp::Timestamp::new(cli.unix);
+
             println!("Carenage start event, time step of {:?} seconds", step.step);
             println!("Start event timestamp is {:?}", start_timestamp.to_string());
+
             let carenaged = Command::new("./target/debug/carenaged")
                 .arg(step.step.to_string())
                 .arg(start_timestamp.to_string())
                 .arg(cli.unix.to_string())
                 .spawn()
                 .expect("Failed to fork carenaged.");
+
             let carenaged_pid = carenaged.id().to_string();
+
             let mut pid_file =
                 File::create("pid.txt").expect("Failed to create file to save child process PID.");
+
             pid_file
                 .write_all(carenaged_pid.as_bytes())
                 .expect("Failed to save child process PID in file.");
@@ -36,16 +41,21 @@ fn main() {
             let system = System::new_all();
 
             let printable_stop_timestamp = stop_timestamp.to_string();
+
             println!("Carenage stop event");
             println!("Stop event timestamp is {:?}", printable_stop_timestamp);
+
             let pid_file = std::fs::read_to_string("pid.txt")
                 .expect("Failed to open file with saved child process PID.");
+
             let pid = pid_file
                 .parse::<u32>()
                 .expect("Failed to convert pid.txt contents to u32.");
+
             let carenaged_process = system
                 .process(Pid::from_u32(pid))
                 .expect("Failed to get process with given PID");
+
             carenaged_process
                 .kill_with(Signal::Term)
                 .expect("Failed to kill with SIGTERM");
