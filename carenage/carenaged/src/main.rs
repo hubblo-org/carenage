5012b62cee7e3014b8e93407475801a5ea7fa59f
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-08-27 17:52:03 +0200

    feat: insert project name and start_date from daemon

--- carenage/carenaged/src/main.rs
+++ carenage/carenaged/src/main.rs
@@ -1,8 +1,8 @@
+use chrono::Utc;
 use database::{
     deserialize_boagent_json, format_hardware_data, get_db_connection_pool, insert_device_metadata,
     insert_dimension_table_metadata, query_boagent, timestamp::Timestamp,
 };
-use dotenv::var;
 use serde_json::json;
 use std::{env, process};
 use tokio::signal::unix::{signal, SignalKind};
@@ -10,13 +10,20 @@ use tokio::time::{self, Duration};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Started carenage daemon with PID: {}", process::id());
+
     let mut sigterm = signal(SignalKind::terminate())?;
     let args: Vec<String> = env::args().collect();
-    let time_step: u64 = args[1].parse().expect("Failed to parse time_step.");
+    let time_step: u64 = args[1]
+        .parse()
+        .expect("time_step variable should be parsable.");
     let start_time_str = args[2].to_string();
     let is_unix_set: bool = args[3]
         .parse()
-        .expect("Failed to parse is_unix_set variable.");
+        .expect("is_unix_set variable should be parsable.");
+    let is_init_set: bool = args[4]
+        .parse()
+        .expect("is_init_set variable should be parsable.");
 
     println!("Time step is : {} seconds.", time_step);
     println!("Start timestamp is {}.", start_time_str);
@@ -35,7 +42,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         )),
     };
 
-    println!("Started carenage daemon with PID: {}", process::id());
+    match is_init_set {
+        true => {
+            let _ = insert_project_metadata().await;
+            print!("Project initialization, inserted project metadata into Carenage database.")
+        }
+        false => (),
+    }
+
 
     // TODO : first query to get hardware_data, format it, send to database with other project
     // metadata
@@ -64,6 +78,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+async fn insert_project_metadata() -> Result<(), Box<dyn std::error::Error>> {
+    let project_root_path = std::env::current_dir().unwrap().join("..");
+    let config = database::Config::check_configuration(&project_root_path)?;
+    let db_pool = get_db_connection_pool(config.database_url).await?;
+    let start_date = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();    
+    let project_data = json!({
+        "name": config.project_name,
+        "start_date": start_date,
+    });
+
+    let insert_project_data =
+        insert_dimension_table_metadata(db_pool.acquire().await?, "projects", project_data).await;
+    match insert_project_data {
+        Ok(insert_project_data) => println!(
+            "Inserted project metadata into database, affected rows: {}",
+            insert_project_data.rows_affected()
+        ),
+        Err(err) => {
+            eprintln!(
+                "Error while processing first query to project table: {}",
+                err
+            );
+            process::exit(0x0100)
+        }
+    };
+    Ok(())
+}
 
 async fn query_and_insert_data(
     start_time: Timestamp,
@@ -88,29 +129,12 @@ async fn query_and_insert_data(
 
     match fetch_hardware {
         true => {
-            let project_data = json!({
-                "name": config.project_name,
-                "start_date": "to_implement",
-                "stop_date": "to_implement",
-            });
-            let insert_project_data =
-                insert_dimension_table_metadata(db_pool.acquire().await?, "projects", project_data)
-                    .await;
-            match insert_project_data {
-                Ok(insert_project_data) => println!(
-                    "Inserted project metadata into database, affected rows: {}",
-                    insert_project_data.rows_affected()
-                ),
-                Err(err) => {
-                    eprintln!(
-                        "Error while processing first query to project table: {}",
-                        err
-                    );
-                    process::exit(0x0100)
-                }
-            };
-            let device_data =
-                format_hardware_data(deserialized_response, config.device_name, config.location, config.lifetime)?;
+            let device_data = format_hardware_data(
+                deserialized_response,
+                config.device_name,
+                config.location,
+                config.lifetime,
+            )?;
             let insert_device_data =
                 insert_device_metadata(db_pool.acquire().await?, device_data).await;
             match insert_device_data {
