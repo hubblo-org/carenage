d8de2361cc4b24b077932993a8601415ae2c881b
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-08-29 16:16:57 +0200

    chore: create library file for carenaged

--- carenage/carenaged/src/carenaged.rs
+++ carenage/carenaged/src/carenaged.rs
@@ -38,3 +38,76 @@ impl DaemonArgs {
         })
     }
 }
+
+pub async fn insert_project_metadata(
+    start_timestamp: Timestamp,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let project_root_path = std::env::current_dir().unwrap().join("..");
+    let config = database::Config::check_configuration(&project_root_path)?;
+    let db_pool = get_db_connection_pool(config.database_url).await?;
+    let project_data = json!({
+        "name": config.project_name,
+        "start_date": start_timestamp.to_string(),
+    });
+
+    let insert_project_data =
+        insert_dimension_table_metadata(db_pool.acquire().await?, "projects", project_data).await;
+    match insert_project_data {
+        Ok(insert_project_data) => println!(
+            "Inserted project metadata into database, affected rows: {}",
+            insert_project_data.rows_affected()
+        ),
+        Err(err) => {
+            eprintln!(
+                "Error while processing first query to project table: {}",
+                err
+            );
+            process::exit(0x0100)
+        }
+    };
+    Ok(())
+}
+
+pub async fn query_and_insert_data(
+    start_time: Timestamp,
+    unix_flag: UnixFlag,
+    fetch_hardware: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let project_root_path = std::env::current_dir().unwrap().join("..");
+    let config = database::Config::check_configuration(&project_root_path)?;
+
+    let end_time = Timestamp::new(unix_flag);
+    let response = query_boagent(
+        config.boagent_url,
+        start_time,
+        end_time,
+        fetch_hardware,
+        config.location.clone(),
+        config.lifetime,
+    )
+    .await?;
+    let deserialized_response = deserialize_boagent_json(response).await?;
+    let db_pool = get_db_connection_pool(config.database_url).await?;
+
+    if fetch_hardware {
+        let device_data = format_hardware_data(
+            deserialized_response,
+            config.device_name,
+            config.location,
+            config.lifetime,
+        )?;
+        let insert_device_data =
+            insert_device_metadata(db_pool.acquire().await?, device_data).await;
+        match insert_device_data {
+            Ok(()) => (),
+            Err(err) => {
+                eprintln!(
+                    "Error while processing first query to device table: {}",
+                    err
+                );
+                process::exit(0x0100)
+            }
+        }
+    };
+    Ok(())
+}
