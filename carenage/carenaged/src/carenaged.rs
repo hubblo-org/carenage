f73b034170f70113f22811d848ac1e8fa1b073cc
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-10-02 16:23:40 +0200

    feat: add device_id after device insertion to id vector

--- carenage/carenaged/src/carenaged.rs
+++ carenage/carenaged/src/carenaged.rs
@@ -1,13 +1,9 @@
 use database::boagent::{deserialize_boagent_json, query_boagent, Config, HardwareData};
 use database::ci::GitlabVariables;
-use database::database::{
-    format_hardware_data, get_db_connection_pool, insert_device_metadata,
-};
 use database::tables::{CarenageRow, Insert};
 use database::timestamp::{Timestamp, UnixFlag};
 use sqlx::types::uuid;
 use std::env;
-use std::process;
 
 pub struct DaemonArgs {
     pub time_step: u64,
@@ -36,23 +32,24 @@ pub async fn insert_project_metadata(
     gitlab_vars: GitlabVariables,
     start_timestamp: Timestamp,
 ) -> Result<Vec<uuid::Uuid>, Box<dyn std::error::Error>> {
+    let project_rows = CarenageRow::Project.insert(start_timestamp, None).await?;
+    let project_id = CarenageRow::Project
+        .get_id(project_rows, Some(gitlab_vars.project_path.clone()))
+        .await?;
 
-    let project_rows = CarenageRow::Project.insert(start_timestamp).await?;
-    let project_id = CarenageRow::Project.get_id(project_rows, Some(gitlab_vars.project_path.clone())).await?;
-
-    let workflow_rows = CarenageRow::Workflow.insert(start_timestamp).await?;
+    let workflow_rows = CarenageRow::Workflow.insert(start_timestamp, None).await?;
     let workflow_id = CarenageRow::Workflow.get_id(workflow_rows, None).await?;
 
-    let pipeline_rows = CarenageRow::Pipeline.insert(start_timestamp).await?;
+    let pipeline_rows = CarenageRow::Pipeline.insert(start_timestamp, None).await?;
     let pipeline_id = CarenageRow::Pipeline.get_id(pipeline_rows, None).await?;
 
-    let job_rows = CarenageRow::Job.insert(start_timestamp).await?;
+    let job_rows = CarenageRow::Job.insert(start_timestamp, None).await?;
     let job_id = CarenageRow::Job.get_id(job_rows, None).await?;
 
-    let run_rows = CarenageRow::Run.insert(start_timestamp).await?;
+    let run_rows = CarenageRow::Run.insert(start_timestamp, None).await?;
     let run_id = CarenageRow::Run.get_id(run_rows, None).await?;
 
-    let task_rows = CarenageRow::Task.insert(start_timestamp).await?;
+    let task_rows = CarenageRow::Task.insert(start_timestamp, None).await?;
     let task_id = CarenageRow::Task.get_id(task_rows, None).await?;
 
     let id_vector: Vec<uuid::Uuid> = vec![
@@ -68,11 +65,11 @@ pub async fn insert_project_metadata(
 
 // Will return device_id on first_query, implement option for fn result
 pub async fn query_and_insert_data(
-    metadata_ids: Option<&Vec<uuid::Uuid>>,
+    mut ids: Option<&mut Vec<uuid::Uuid>>,
     start_time: Timestamp,
     unix_flag: UnixFlag,
     fetch_hardware: HardwareData,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<&Vec<uuid::Uuid>, Box<dyn std::error::Error>> {
     let project_root_path = std::env::current_dir().unwrap().join("..");
     let config = Config::check_configuration(&project_root_path)?;
 
@@ -86,28 +83,19 @@ pub async fn query_and_insert_data(
         config.lifetime,
     )
     .await?;
-    let deserialized_response = deserialize_boagent_json(response).await?;
-    let db_pool = get_db_connection_pool(config.database_url).await?;
+    let deserialized_boagent_response = deserialize_boagent_json(response).await?;
 
     if let HardwareData::Inspect = fetch_hardware {
-        let device_data = format_hardware_data(
-            deserialized_response,
-            config.device_name,
-            config.location,
-            config.lifetime,
-        )?;
-        let insert_device_data =
-            insert_device_metadata(db_pool.acquire().await?, device_data).await;
-        match insert_device_data {
-            Ok(_insert_device_data) => println!("Inserted device data into database."),
-            Err(err) => {
-                eprintln!(
-                    "Error while processing first query to device table: {}",
-                    err
-                );
-                process::exit(0x0100)
-            }
-        }
-    };
-    Ok(())
+        let insert_device_data = CarenageRow::Device
+            .insert(start_time, Some(deserialized_boagent_response))
+            .await?;
+        let device_id = CarenageRow::Device.get_id(insert_device_data, None).await?;
+        ids
+            .as_mut()
+            .expect("Device_id should be added to other primary keys.")
+            .push(device_id)
+    } else {
+        todo!()
+    }
+    Ok(ids.expect("Primary keys should be available after database insertion."))
 }
