64388381f0222bdb21b805c1bae3353815344112
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-11-06 18:50:22 +0100

    refactor: implement api response with different process structs

--- carenage/api/src/api.rs
+++ carenage/api/src/api.rs
@@ -1,134 +1,89 @@
-use database::metrics::ProcessEmbeddedImpactValues;
-use database::{database::Ids, metrics::Metrics};
-use log::{debug, info};
-use sqlx::postgres::PgRow;
-use sqlx::Row;
+use database::database::Record;
+use std::collections::HashSet;
 
-pub struct ProcessRecord {
-    pub pid: i32,
-    pub exe: String,
-    pub cmdline: String,
-    pub metrics: Metrics,
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct ProcessInfo {
+    pub process_pid: i32,
+    pub process_exe: String,
+    pub process_cmdline: String,
 }
+
+#[derive(Debug)]
+pub struct ProcessMetrics {
+    pub metric_name: String,
+    pub metric_values: Vec<f64>,
+}
+
+#[derive(Debug)]
+pub struct ProcessRecord {
+    pub process: ProcessInfo,
+    pub metrics: Vec<ProcessMetrics>,
+}
+
+#[derive(Debug)]
 pub struct ApiResponse {
     pub project_name: String,
-    pub ids: Ids,
     pub processes: Vec<ProcessRecord>,
 }
 
 pub struct ApiResponseBuilder(ApiResponse);
 
-pub struct ProcessRecordBuilder(ProcessRecord);
-
-impl ProcessRecordBuilder {
-    pub fn new(metric_row: &PgRow) -> Self {
-        let process_cpu_embedded_impacts = ProcessEmbeddedImpactValues {
-            adp_max_impact: metric_row.get("cpu_adp_max_impact"),
-            adp_min_impact: metric_row.get("cpu_adp_min_impact"),
-            adp_average_impact: metric_row.get("cpu_adp_average_impact"),
-            gwp_max_impact: metric_row.get("cpu_gwp_max_impact"),
-            gwp_min_impact: metric_row.get("cpu_gwp_min_impact"),
-            gwp_average_impact: metric_row.get("cpu_gwp_average_impact"),
-            pe_max_impact: metric_row.get("cpu_pe_max_impact"),
-            pe_min_impact: metric_row.get("cpu_pe_min_impact"),
-            pe_average_impact: metric_row.get("cpu_pe_average_impact"),
-        };
-        let process_ram_embedded_impacts = ProcessEmbeddedImpactValues {
-            adp_max_impact: metric_row.get("ram_adp_max_impact"),
-            adp_min_impact: metric_row.get("ram_adp_min_impact"),
-            adp_average_impact: metric_row.get("ram_adp_average_impact"),
-            gwp_max_impact: metric_row.get("ram_gwp_max_impact"),
-            gwp_min_impact: metric_row.get("ram_gwp_min_impact"),
-            gwp_average_impact: metric_row.get("ram_gwp_average_impact"),
-            pe_max_impact: metric_row.get("ram_pe_max_impact"),
-            pe_min_impact: metric_row.get("ram_pe_min_impact"),
-            pe_average_impact: metric_row.get("ram_pe_average_impact"),
-        };
-
-        let process_ssd_embedded_impacts = match metric_row.try_get("ssd_adp_max_impact") {
-            Ok(value) => Some(ProcessEmbeddedImpactValues {
-                adp_max_impact: value,
-                adp_min_impact: metric_row.get("ssd_adp_min_impact"),
-                adp_average_impact: metric_row.get("ssd_adp_average_impact"),
-                gwp_max_impact: metric_row.get("ssd_gwp_max_impact"),
-                gwp_min_impact: metric_row.get("ssd_gwp_min_impact"),
-                gwp_average_impact: metric_row.get("ssd_gwp_average_impact"),
-                pe_max_impact: metric_row.get("ssd_pe_max_impact"),
-                pe_min_impact: metric_row.get("ssd_pe_min_impact"),
-                pe_average_impact: metric_row.get("ssd_pe_average_impact"),
-            }),
-            Err(error) => {
-                info!("No SSD information for this process!");
-                debug!("{}", error);
-                None
-            }
-        };
-
-        let process_hdd_embedded_impacts = match metric_row.try_get("hdd_adp_max_impact") {
-            Ok(value) => Some(ProcessEmbeddedImpactValues {
-                adp_max_impact: value,
-                adp_min_impact: metric_row.get("hdd_adp_min_impact"),
-                adp_average_impact: metric_row.get("hdd_adp_average_impact"),
-                gwp_max_impact: metric_row.get("hdd_gwp_max_impact"),
-                gwp_min_impact: metric_row.get("hdd_gwp_min_impact"),
-                gwp_average_impact: metric_row.get("hdd_gwp_average_impact"),
-                pe_max_impact: metric_row.get("hdd_pe_max_impact"),
-                pe_min_impact: metric_row.get("hdd_pe_min_impact"),
-                pe_average_impact: metric_row.get("hdd_pe_average_impact"),
-            }),
-            Err(error) => {
-                info!("No HDD information for this process!");
-                debug!("{}", error);
-                None
-            }
-        };
-
-        let process_metrics = Metrics {
-            average_power_measured_w: metric_row.get("average_power_measured_w"),
-            cpu_usage_percentage: metric_row.get("cpu_usage_percentage"),
-            disk_usage_read_bytes: metric_row.get("disk_usage_read_bytes"),
-            disk_usage_write_bytes: metric_row.get("disk_usage_write_bytes"),
-            embedded_emissions_kgc02eq: metric_row.get("embedded_emissions_kgc02eq"),
-            embedded_abiotic_resources_depletion_kgsbeq: metric_row
-                .get("embedded_abiotic_resources_depletion_kgsbeq"),
-            embedded_primary_energy_mj: metric_row.get("embedded_primary_energy_mj"),
-            memory_usage_bytes: metric_row.get("memory_usage_bytes"),
-            memory_virtual_usage_bytes: metric_row.get("memory_virtual_usage_bytes"),
-            process_cpu_embedded_impacts: Some(process_cpu_embedded_impacts),
-            process_ram_embedded_impacts: Some(process_ram_embedded_impacts),
-            process_ssd_embedded_impacts,
-            process_hdd_embedded_impacts,
-            total_operational_abiotic_resources_depletion_kgsbeq: metric_row
-                .get("total_operational_abiotic_resources_depletion_kgsbeq"),
-            total_operational_emission_kgc02eq: metric_row
-                .get("total_operational_emission_kgc02eq"),
-            total_primary_energy_consumed_mj: metric_row.get("total_primary_energy_consumed_mj"),
-        };
-
-        ProcessRecordBuilder(ProcessRecord {
-            pid: metric_row.get("pid"),
-            exe: metric_row.get("exe"),
-            cmdline: metric_row.get("cmdline"),
-            metrics: process_metrics,
-        })
-    }
-    pub fn build(self) -> ProcessRecord {
-        self.0
-    }
-}
-
 impl ApiResponseBuilder {
-    pub fn new(metrics_rows: &[PgRow]) -> Self {
-        let processes_records: Vec<ProcessRecord> = metrics_rows
+    pub fn new(records: &[Record], project_name: String) -> Self {
+        let mut metrics_names: Vec<String> = records
             .iter()
-            .map(|metric_row| ProcessRecordBuilder::new(metric_row).build())
+            .map(|record| record.metric.clone())
+            .collect::<HashSet<String>>()
+            .into_iter()
+            .collect();
+
+        let mut processes_infos: Vec<ProcessInfo> = records
+            .iter()
+            .map(|record| ProcessInfo {
+                process_pid: record.pid,
+                process_exe: record.exe.clone(),
+                process_cmdline: record.cmdline.clone(),
+            })
+            .collect::<HashSet<ProcessInfo>>()
+            .into_iter()
+            .collect();
+
+        processes_infos.sort();
+        metrics_names.sort();
+
+        let processes: Vec<ProcessRecord> = processes_infos
+            .into_iter()
+            .map(|process| {
+                let process_metrics: Vec<ProcessMetrics> = metrics_names
+                    .clone()
+                    .into_iter()
+                    .map(|metric_name| {
+                        let metric_values = records
+                            .iter()
+                            .filter(|record| {
+                                record.pid == process.process_pid && record.metric == metric_name
+                            })
+                            .map(|record| record.value)
+                            .collect::<Vec<f64>>();
+
+                        ProcessMetrics {
+                            metric_name,
+                            metric_values,
+                        }
+                    })
+                    .collect();
+                ProcessRecord {
+                    process,
+                    metrics: process_metrics,
+                }
+            })
             .collect();
         ApiResponseBuilder(ApiResponse {
-            processes: processes_records,
-            project_name: todo!(),
-            ids: todo!(),
+            project_name,
+            processes,
         })
     }
+
     pub fn build(self) -> ApiResponse {
         self.0
     }
