d27cdff077092338c530cb86b12d5b4241b7138c
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-11-13 14:55:19 +0100

    feat: api server process

--- carenage/api/src/main.rs
+++ carenage/api/src/main.rs
@@ -1,7 +1,23 @@
-use api::api::{app, get_run};
+use api::api::app;
+use axum::Extension;
+use database::boagent::Config;
+use sqlx::postgres::PgPoolOptions;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    // axum::serve(listener, app).await.unwrap();
+    let project_root_path = std::env::current_dir().unwrap().join("..");
+    let config = Config::check_configuration(&project_root_path)
+        .expect("Configuration fields should be parsable.");
+
+    let db_pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.database_url)
+        .await
+        .expect("Failed to connect to database");
+
+    let app = app().layer(Extension(db_pool));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
