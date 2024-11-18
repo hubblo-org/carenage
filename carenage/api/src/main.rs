ef5cd692327bb80c9d1042eed1048fbd1a6f7034
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-11-18 15:22:48 +0100

    feat: add tracing logs for http responses

--- carenage/api/src/main.rs
+++ carenage/api/src/main.rs
@@ -3,9 +3,12 @@ use axum::Extension;
 use database::boagent::Config;
 use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
     let project_root_path = std::env::current_dir().unwrap().join("..");
     let config = Config::check_configuration(&project_root_path)
         .expect("Configuration fields should be parsable.");
@@ -16,7 +19,11 @@ async fn main() {
         .await
         .expect("Failed to connect to database");
 
-    let app = app().layer(Extension(db_pool));
+    let app = app().layer(Extension(db_pool)).layer(
+        TraceLayer::new_for_http()
+            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+    );
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
