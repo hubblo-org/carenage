54e54ece7199c517a8e43329be8a13c515ef55e9
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-11-25 15:47:05 +0100

    feat: add title, labels and height for graph

--- dashboard/src/lib/create-graph.svelte.ts
+++ dashboard/src/lib/create-graph.svelte.ts
@@ -1,13 +1,18 @@
 import type { MetricValues } from "../types/carenage";
 
-export async function createGraph(metricValues: MetricValues) {
+export async function createGraph(metricValues: MetricValues, metric: string) {
   const Dygraph = (await import("dygraphs")).default;
   const values = metricValues.map((value) => {
     const date = new Date(value[0]);
     return [date, value[1]];
   });
+  console.log(metric);
   const dygraph = new Dygraph("graph", values, {
-    labels: ["timestamp", "metric_value"]
+    labels: ["timestamp", "metric_value"],
+    title: "Metric values for selected process and metric",
+    xlabel: "Time",
+    ylabel: `${metric}`,
+    height: 700
   });
   return dygraph;
 }
