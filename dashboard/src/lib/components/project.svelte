e51bde0ab796df16261418e70b56469aee8ca7c2
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-02 11:14:42 +0100

    feat: add section for project's aggregated metric values

--- dashboard/src/lib/components/project.svelte
+++ dashboard/src/lib/components/project.svelte
@@ -11,30 +11,35 @@
 </script>
 
 <main>
-  <h2>Project: {project.project_name}</h2>
-  <p>Project created on: {formattedDate}</p>
-  <table>
-    <caption>List of executed CI pipelines for the project</caption>
-    <thead
-      ><tr><th scope="col">Pipeline ID</th><th scope="col">Pipeline date of execution</th></tr
-      ></thead
-    >
-    <tbody
-      >{#each project.runs as run}<tr
-          ><td><a href="/pipelines/{run.pipeline_id}">{run.pipeline_id}</a></td>
-          <td>{run.started_at}</td></tr
-        >
-      {/each}</tbody
-    >
-  </table>
-  <table>
-    <caption>List of executed CI runs for the project</caption><thead
-      ><tr><th scope="col">Run ID</th><th scope="col">Run date of execution</th></tr></thead
-    >
-    <tbody>
-      {#each project.runs as run}
-        <tr><td><a href="/runs/{run.run_id}">{run.run_id}</a></td><td>{run.started_at}</td></tr>
-      {/each}
-    </tbody>
-  </table>
+  <section class="project-metadata" aria-labelledby="project-metadata-heading">
+    <h2 id="project-metadata-heading">Project: {project.project_name}</h2>
+    <p>Project created on: {formattedDate}</p>
+    <table>
+      <caption>List of executed CI pipelines for the project</caption>
+      <thead
+        ><tr><th scope="col">Pipeline ID</th><th scope="col">Pipeline date of execution</th></tr
+        ></thead
+      >
+      <tbody
+        >{#each project.runs as run}<tr
+            ><td><a href="/pipelines/{run.pipeline_id}">{run.pipeline_id}</a></td>
+            <td>{run.started_at}</td></tr
+          >
+        {/each}</tbody
+      >
+    </table>
+    <table>
+      <caption>List of executed CI runs for the project</caption><thead
+        ><tr><th scope="col">Run ID</th><th scope="col">Run date of execution</th></tr></thead
+      >
+      <tbody>
+        {#each project.runs as run}
+          <tr><td><a href="/runs/{run.run_id}">{run.run_id}</a></td><td>{run.started_at}</td></tr>
+        {/each}
+      </tbody>
+    </table>
+  </section>
+  <section class="aggregated-metric-values" aria-labelledby="aggregated-metric-values-heading">
+    <h2 id="aggregated-metric-values-heading">Project's aggregated metric values</h2>
+  </section>
 </main>
