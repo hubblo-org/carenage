b4943a7bde486f7261370cd750c831c61407fda4
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-03 17:57:47 +0100

    refactor: change type to cipipeline, for future development with aggregated metrics for pipeline

--- dashboard/src/routes/pipelines/[id]/+page.server.ts
+++ dashboard/src/routes/pipelines/[id]/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from "./$types";
-import type { CiPipelineMetadata } from "$lib/types/carenage";
+import type { CiPipeline } from "$lib/types/carenage";
 
 export const load: PageServerLoad = async ({ params }) => {
   return { pipeline: await fetchPipeline(params.id) };
@@ -7,7 +7,7 @@ export const load: PageServerLoad = async ({ params }) => {
 
 async function fetchPipeline(pipeline_id: string) {
   const response = await fetch(`https://api.carenage.hubblo.org/pipelines/${pipeline_id}`);
-  const pipeline = (await response.json()) as Promise<CiPipelineMetadata>;
+  const pipeline = (await response.json()) as Promise<CiPipeline>;
 
   return pipeline;
 }
