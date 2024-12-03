9e8122febd44b92537a74eaa28adb9977cbfef45
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-03 17:58:34 +0100

    feat: server code for run routes

--- dashboard/src/routes/runs/[id]/+page.server.ts
+++ dashboard/src/routes/runs/[id]/+page.server.ts
@@ -1,8 +1,13 @@
 import type { PageServerLoad } from "./$types";
+import type { CiRun } from "$lib/types/carenage";
 
-export const load: PageServerLoad = async ({ fetch, params }) => {
-  const response = await fetch(`https://api.carenage.boukin.org/${params.id}`);
-  const run = await response.json();
-
-  return { run };
+export const load: PageServerLoad = async ({ params }) => {
+  return { run: await fetchRun(params.id) };
 };
+
+async function fetchRun(run_id: string) {
+  const response = await fetch(`https://api.carenage.hubblo.org/runs/${run_id}`);
+  const run = (await response.json()) as Promise<CiRun>;
+
+  return run;
+}
--- dashboard/src/routes/runs/[id]/+page.svelte
+++ dashboard/src/routes/runs/[id]/+page.svelte
@@ -1,22 +1,8 @@
 <script lang="ts">
+  import type { PageData } from "./$types";
   import Run from "$lib/components/run.svelte";
-  import Header from "$lib/components/header.svelte";
-  import type { CiRun, Process } from "../../types/carenage";
 
-  import processes from "../../tests/fixtures/run.json";
-
-  const processes_data: Array<Process> = processes.processes;
-
-  const run: CiRun = {
-    project_name: "hubblo/carenage",
-    pipeline_id: 1520057997,
-    run_id: 8228228299,
-    job_name: "test_for_merge_request",
-    job_status: "success",
-    job_duration: 180,
-    processes: processes_data
-  };
+  let { data }: { data: PageData } = $props();
 </script>
 
-<Header {run} />
-<Run {run} />
+<Run run={data.run} />
