c732730ac6077698b5d1fabba68ba83d133f0a4a
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-03 16:36:46 +0100

    build: add handlers and server hook for msw & svelte

--- dashboard/src/mocks/handlers.ts
+++ dashboard/src/mocks/handlers.ts
@@ -1,16 +1,12 @@
 import { http, HttpResponse } from "msw";
 import run from "../tests/fixtures/run.json";
+import pipeline from "../tests/fixtures/project_metadata.json";
 
 export const handlers = [
   http.get("https://api.carenage.hubblo.org/runs/c86fe1e5-8828-4f53-822b-df7e2ced37db", () => {
-    return HttpResponse.json({
-      project_name: "hubblo/carenage",
-      pipeline_id: 1520057997,
-      run_id: 8228228299,
-      job_name: "test_for_merge_request",
-      job_status: "success",
-      job_duration: 180,
-      processes: run.processes
-    });
+    return HttpResponse.json(run);
+  }),
+  http.get("https://api.carenage.hubblo.org/projects/3a1f9a71-fdd2-4e89-9769-70cfb731a02d", () => {
+    return HttpResponse.json(pipeline);
   })
 ];
