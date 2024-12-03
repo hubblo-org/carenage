5dec1c4bd43990f5c3f38f36549537df6b360e2f
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-03 17:05:40 +0100

    test: handling routing to pipeline page from project page

--- dashboard/src/mocks/handlers.ts
+++ dashboard/src/mocks/handlers.ts
@@ -1,12 +1,16 @@
 import { http, HttpResponse } from "msw";
 import run from "../tests/fixtures/run.json";
-import pipeline from "../tests/fixtures/project_metadata.json";
+import project from "../tests/fixtures/project_metadata.json";
+import pipeline from "../tests/fixtures/pipeline.json";
 
 export const handlers = [
   http.get("https://api.carenage.hubblo.org/runs/c86fe1e5-8828-4f53-822b-df7e2ced37db", () => {
     return HttpResponse.json(run);
   }),
   http.get("https://api.carenage.hubblo.org/projects/3a1f9a71-fdd2-4e89-9769-70cfb731a02d", () => {
+    return HttpResponse.json(project);
+  }),
+  http.get("https://api.carenage.hubblo.org/pipelines/d199e857-fb0f-46b1-9846-74e53b494740", () => {
     return HttpResponse.json(pipeline);
   })
 ];
--- dashboard/src/tests/e2e/project.test.ts
+++ dashboard/src/tests/e2e/project.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 const projectId = "3a1f9a71-fdd2-4e89-9769-70cfb731a02d";
+const pipelineId = 1520057997;
 
 test("renders the associated project page for the given id", async ({ page }) => {
   await page.goto(`/projects/${projectId}`);
@@ -14,4 +15,22 @@ test("renders the associated project page for the given id", async ({ page }) =>
   await expect(projectNameHeading).toBeVisible();
   await expect(projectPipelinesTable).toBeVisible();
   await expect(projectRunsTable).toBeVisible();
+});
+
+test("routes to the select pipeline page after clicking on the selected pipeline link", async ({
+  page
+}) => {
+  await page.goto(`/projects/${projectId}`);
+  const projectPipelinesTables = page.getByRole("table", {
+    name: "List of executed CI pipelines for the project"
+  });
+  const pipelineLink = projectPipelinesTables.getByRole("link", {
+    name: `${pipelineId}`
+  });
+  await pipelineLink.click();
+
+  const pipelineMetadataHeading = page.getByRole("heading", {
+    name: `Pipeline #${pipelineId} metadata`
+  });
+  await expect(pipelineMetadataHeading).toBeVisible();
 });
