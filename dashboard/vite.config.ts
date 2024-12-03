302c70629a7f52676208a4288c55b40f5f56c172
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-03 17:09:34 +0100

    build: ignore e2e folder for vitest

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vitest/config';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { svelteTesting } from '@testing-library/svelte/vite';
+import { defineConfig } from "vitest/config";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { svelteTesting } from "@testing-library/svelte/vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), svelteTesting()],
+  plugins: [sveltekit(), svelteTesting()],
 
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}'],
-		environment: 'jsdom',
-		setupFiles: ['./vitest-setup.ts']
-	}
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+    exclude: ["src/tests/e2e"],
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup.ts"]
+  }
 });
