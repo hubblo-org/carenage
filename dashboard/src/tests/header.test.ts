f4a3943a1bbc224d18560b79180f854d22c8a77f
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-04 13:09:56 +0100

    test: handling display of executed pipelines and runs

--- dashboard/src/tests/header.test.ts
+++ dashboard/src/tests/header.test.ts
@@ -1,11 +1,11 @@
 import { render, screen } from "@testing-library/svelte";
 import { beforeEach, describe, expect, it } from "vitest";
 import Header from "$lib/components/header.svelte";
-import run from "./fixtures/run.json";
+import project_metadata from "./fixtures/project_metadata.json";
 
 describe("header test suite", () => {
   beforeEach(() => {
-    render(Header, { props: { run: run } });
+    render(Header, { props: { project: project_metadata } });
   });
   it("displays a heading with the name of Carenage", () => {
     const carenageHeading = screen.getByRole("heading", { name: "Carenage" });
@@ -13,11 +13,17 @@ describe("header test suite", () => {
   });
   it("displays the project's metadata", () => {
     /* Number of runs, of pipelines, project's repository, list of last pipelines with links*/
-    const numberOfPipelines = screen.getByText("Number of pipelines executed");
-    const numberOfRuns = screen.getByText("Number of job runs executed");
-    const projectRepoLink = screen.getByRole("link", { name: `${run.project_name}` });
-    expect(numberOfPipelines).toBeVisible();
-    expect(numberOfRuns).toBeVisible();
+    const numberOfPipelines = project_metadata.pipelines.length;
+    const numberOfRuns = project_metadata.pipelines
+      .map((pipeline) => pipeline.runs.length)
+      .reduce((acc, run) => run + acc, 0);
+    const numberOfPipelinesText = screen.getByText(
+      `Number of pipelines executed: ${numberOfPipelines}`
+    );
+    const numberOfRunsText = screen.getByText(`Number of job runs executed: ${numberOfRuns}`);
+    const projectRepoLink = screen.getByRole("link", { name: `${project_metadata.project_name}` });
+    expect(numberOfPipelinesText).toBeVisible();
+    expect(numberOfRunsText).toBeVisible();
     expect(projectRepoLink).toBeVisible();
   });
 });
