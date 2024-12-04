ab0c1b344dab005ec2ae91e4209a1343605cba97
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-04 13:08:42 +0100

    feat: set projectid cookie in server code for pages route

--- dashboard/src/routes/projects/[id]/+page.server.ts
+++ dashboard/src/routes/projects/[id]/+page.server.ts
@@ -1,12 +1,13 @@
 import type { PageServerLoad } from "./$types";
 import type { ProjectMetadata } from "$lib/types/carenage";
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, cookies }) => {
+  cookies.set("projectid", params.id, { path: "/" });
   return { project: await fetchProject(params.id) };
 };
 
-async function fetchProject(project_id: string) {
-  const response = await fetch(`https://api.carenage.hubblo.org/projects/${project_id}`);
+async function fetchProject(projectId: string) {
+  const response = await fetch(`https://api.carenage.hubblo.org/projects/${projectId}`);
   const project = (await response.json()) as Promise<ProjectMetadata>;
 
   return project;
