07314e107d6e45edefbe3152e64d5c93e1061888
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-04 17:37:14 +0100

    test: add navigation element to header

--- dashboard/src/tests/header.test.ts
+++ dashboard/src/tests/header.test.ts
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/svelte";
+import { render, screen, within } from "@testing-library/svelte";
 import { beforeEach, describe, expect, it } from "vitest";
 import Header from "$lib/components/header.svelte";
-import project_metadata from "./fixtures/project_metadata.json";
+import projectMetadata from "./fixtures/project_metadata.json";
 
 describe("header test suite", () => {
   beforeEach(() => {
-    render(Header, { props: { project: project_metadata } });
+    render(Header, { props: { project: projectMetadata } });
   });
   it("displays a heading with the name of Carenage", () => {
     const carenageHeading = screen.getByRole("heading", { name: "Carenage" });
@@ -13,17 +13,36 @@ describe("header test suite", () => {
   });
   it("displays the project's metadata", () => {
     /* Number of runs, of pipelines, project's repository, list of last pipelines with links*/
-    const numberOfPipelines = project_metadata.pipelines.length;
-    const numberOfRuns = project_metadata.pipelines
+    const numberOfPipelines = projectMetadata.pipelines.length;
+    const numberOfRuns = projectMetadata.pipelines
       .map((pipeline) => pipeline.runs.length)
       .reduce((acc, run) => run + acc, 0);
     const numberOfPipelinesText = screen.getByText(
       `Number of pipelines executed: ${numberOfPipelines}`
     );
     const numberOfRunsText = screen.getByText(`Number of job runs executed: ${numberOfRuns}`);
-    const projectRepoLink = screen.getByRole("link", { name: `${project_metadata.project_name}` });
+    const projectRepoLink = screen.getByRole("link", { name: `${projectMetadata.project_name}` });
     expect(numberOfPipelinesText).toBeVisible();
     expect(numberOfRunsText).toBeVisible();
     expect(projectRepoLink).toBeVisible();
   });
+  it("displays a navigation element allowing the user to go to the project page showing its metadata and metrics", () => {
+    const navbar = screen.getByRole("navigation");
+    const projectPageLink = within(navbar).getByRole("link", {
+      name: "Project summary and metrics"
+    });
+    expect(projectPageLink).toBeVisible();
+  });
+
+  // Not sure how to mock $page.data to test different behaviour for this component
+  /* it("displays a link to the pipeline related to the run in the navigation element", () => {
+    vi.mock("$app/stores", async () => {
+      return { page: { data: { run: run } } };
+    });
+    const navbar = screen.getByRole("navigation");
+    const pipelinePageLink = within(navbar).getByRole("link", {
+      name: "Pipeline summary and metrics"
+    });
+    expect(pipelinePageLink).toBeVisible();
+  }); */
 });
