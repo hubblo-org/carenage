1104bde11a2cb3f17c6091fc75743bb4415795e5
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-05 18:23:06 +0100

    refactor: add undefined condition for projectid cookie

--- dashboard/src/hooks.server.ts
+++ dashboard/src/hooks.server.ts
@@ -7,10 +7,12 @@ if (dev) {
 }
 
 export const handle: Handle = async ({ event, resolve }) => {
-  if (event.url.pathname.startsWith("/projects")) {
-    const splitPathName = event.url.pathname.split("/");
-    const projectId = splitPathName[2];
-    event.cookies.set("projectid", projectId, { path: "/" });
+  if (event.cookies.get("projectid") == undefined) {
+    if (event.url.pathname.startsWith("/projects")) {
+      const splitPathName = event.url.pathname.split("/");
+      const projectId = splitPathName[2];
+      event.cookies.set("projectid", projectId, { path: "/" });
+    }
   }
   const response = await resolve(event);
   return response;
