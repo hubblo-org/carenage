d7f90efdca97004996035909915e6353bc07a9ea
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-05 18:22:12 +0100

    feat: handling 404 error when passing invalid ID in projects route

--- dashboard/src/routes/projects/[id]/+page.server.ts
+++ dashboard/src/routes/projects/[id]/+page.server.ts
@@ -1,13 +1,24 @@
+import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import type { ProjectMetadata } from "$lib/types/carenage";
 
 export const load: PageServerLoad = async ({ params }) => {
-  return { project: await fetchProject(params.id) };
+  const project: ProjectMetadata = await _fetchProject(params.id);
+  if (!project) {
+    error(404, "Not found");
+  }
+  return { project };
 };
 
-async function fetchProject(projectId: string) {
-  const response = await fetch(`https://api.carenage.hubblo.org/projects/${projectId}`);
-  const project = (await response.json()) as Promise<ProjectMetadata>;
-
-  return project;
+export async function _fetchProject(projectId: string) {
+  try {
+    const response = await fetch(`https://api.carenage.hubblo.org/projects/${projectId}`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const project = await response.json();
+    return project;
+  } catch (err) {
+    console.log(err.message);
+  }
 }
