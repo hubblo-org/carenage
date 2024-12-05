6c4ad9a56cc17a92b0fe1c3ad65f2ae2d5327bc6
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-05 18:22:39 +0100

    feat: add 404 response on projects endpoint for msw

--- dashboard/src/mocks/handlers.ts
+++ dashboard/src/mocks/handlers.ts
@@ -3,14 +3,25 @@ import run from "../tests/fixtures/run.json";
 import project from "../tests/fixtures/project_metadata.json";
 import pipeline from "../tests/fixtures/pipeline.json";
 
+const projectId = "3a1f9a71-fdd2-4e89-9769-70cfb731a02d";
+const runId = "d910fcd3-fef1-4077-9294-efea1975e3fc";
+const pipelineId = "d199e857-fb0f-46b1-9846-74e53b494740";
+const carenageApiBaseUrl = "https://api.carenage.hubblo.org";
+
 export const handlers = [
-  http.get("https://api.carenage.hubblo.org/runs/d910fcd3-fef1-4077-9294-efea1975e3fc", () => {
+  http.get(`${carenageApiBaseUrl}/runs/${runId}`, () => {
     return HttpResponse.json(run);
   }),
-  http.get("https://api.carenage.hubblo.org/projects/3a1f9a71-fdd2-4e89-9769-70cfb731a02d", () => {
+  http.get(`${carenageApiBaseUrl}/projects/${projectId}`, () => {
     return HttpResponse.json(project);
   }),
-  http.get("https://api.carenage.hubblo.org/pipelines/d199e857-fb0f-46b1-9846-74e53b494740", () => {
+  http.get(`${carenageApiBaseUrl}/projects/invalid_id`, () => {
+    return new HttpResponse(null, {
+      status: 404,
+      statusText: "Not found"
+    });
+  }),
+  http.get(`${carenageApiBaseUrl}/pipelines/${pipelineId}`, () => {
     return HttpResponse.json(pipeline);
   })
 ];
